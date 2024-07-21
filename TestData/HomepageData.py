1b566975945c68a18ba9ac6a54516ad7bce82c0d
Sunmathi Dhanasekaran <sunmathiuthra26@gmail.com>
2024-07-21 07:39:54 +0530

    Data page is added

---
+++ TestData/HomepageData.py
@@ -0,0 +1,9 @@
+"""
+HomePageData.py
+"""
+class HomePage:
+   username = "Admin"
+   password = "admin123"
+   url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+   title = "OrangeHRM"
+   dashboard_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
